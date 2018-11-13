8ecf43d5de78874ef3fd6ebbb1acb33b86406101
SC van Nostrand <scvannost@gmail.com>
2018-11-13 15:03:06 -0500

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-  name = 'mutlilang',
+  name = 'multilang',
   packages = ['multilang'],
   version = '0.1.1a2',
   license='MIT',
