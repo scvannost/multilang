0a770665ee5f5ca7448295390bc2136556c5f0fc
SC van Nostrand <scvannost@gmail.com>
2018-11-15 14:46:16 -0500

    Update version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   description = 'Allows for running code in mutliple languages',
   author = 'SC van Nostrand',
   author_email = 'scvannost@gmail.com',
-  download_url = 'https://github.com/scvannost/multilang/archive/0.1.2a1.tar.gz',
+  download_url = 'https://github.com/scvannost/multilang/archive/0.1.2a2.tar.gz',
   url = 'https://github.com/scvannost/multilang',
   keywords = ['R', 'Matlab', 'environment', 'bash', 'multilanguage'],
   install_requires=[
